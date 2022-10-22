4753f67fee3642e7de13fe958a9b9cd1179e2578
Walter <silvawalteralipio@gmail.com>
2022-10-22 20:01:30 -0300

    add table Enderecos e Cinemas

--- Migrations/20221020214227_CriandoTabelaFilmes.Designer.cs
+++ Migrations/20221020214227_CriandoTabelaFilmes.Designer.cs
@@ -8,44 +8,44 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FilmesAPI.Migrations
 {
-    [DbContext(typeof(FilmeContext))]
-    [Migration("20221020214227_CriandoTabelaFilmes")]
-    partial class CriandoTabelaFilmes
+  [DbContext(typeof(AppDbContext))]
+  [Migration("20221020214227_CriandoTabelaFilmes")]
+  partial class CriandoTabelaFilmes
+  {
+    protected override void BuildTargetModel(ModelBuilder modelBuilder)
     {
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
-        {
 #pragma warning disable 612, 618
-            modelBuilder
-                .HasAnnotation("ProductVersion", "6.0.10")
-                .HasAnnotation("Relational:MaxIdentifierLength", 64);
+      modelBuilder
+          .HasAnnotation("ProductVersion", "6.0.10")
+          .HasAnnotation("Relational:MaxIdentifierLength", 64);
 
-            modelBuilder.Entity("Filme", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
+      modelBuilder.Entity("Filme", b =>
+          {
+            b.Property<int>("Id")
+                      .ValueGeneratedOnAdd()
+                      .HasColumnType("int");
 
-                    b.Property<string>("Diretor")
-                        .IsRequired()
-                        .HasColumnType("longtext");
+            b.Property<string>("Diretor")
+                      .IsRequired()
+                      .HasColumnType("longtext");
 
-                    b.Property<int>("Duracao")
-                        .HasColumnType("int");
+            b.Property<int>("Duracao")
+                      .HasColumnType("int");
 
-                    b.Property<string>("Genero")
-                        .IsRequired()
-                        .HasMaxLength(30)
-                        .HasColumnType("varchar(30)");
+            b.Property<string>("Genero")
+                      .IsRequired()
+                      .HasMaxLength(30)
+                      .HasColumnType("varchar(30)");
 
-                    b.Property<string>("Titulo")
-                        .IsRequired()
-                        .HasColumnType("longtext");
+            b.Property<string>("Titulo")
+                      .IsRequired()
+                      .HasColumnType("longtext");
 
-                    b.HasKey("Id");
+            b.HasKey("Id");
 
-                    b.ToTable("Filmes");
-                });
+            b.ToTable("Filmes");
+          });
 #pragma warning restore 612, 618
-        }
     }
+  }
 }
