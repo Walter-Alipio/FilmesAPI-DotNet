27cb12a68f49945385eedc7f2a5b3586857f03cd
Walter <silvawalteralipio@gmail.com>
2022-10-23 09:01:50 -0300

    Resolved MySQL exeption ao tentar usar PUT após add LazyLoadingProxies

--- Controllers/CinemaController.cs
+++ Controllers/CinemaController.cs
@@ -24,7 +24,7 @@ public class CinemaController : ControllerBase
   [HttpGet]
   public IActionResult showCinemas()
   {
-    return Ok(_context.Cinemas);
+    return Ok(_context.Cinemas.ToList());
   }
 
   [HttpGet("{id}")]
